c310771647930c2c32c8829af905ad3734afb59b
cemkurtoglu <cemkurtoglu@mail.com>
2022-10-30 17:38:06 +0300

    #5; health_care app it created.

--- Personalized_healthcare_system_for_neurodegenerative_diseases/urls.py
+++ Personalized_healthcare_system_for_neurodegenerative_diseases/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from healthcare_app.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home_view')
+
 ]
---
+++ healthcare_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HealthcareAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'healthcare_app'
